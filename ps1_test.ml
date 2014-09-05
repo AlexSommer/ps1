1008d817eaadaa445b9cc8e08389f395a49e4b46
Maddie Cripps <mfscripps@gmail.com>
2014-09-05 19:55:36 -0400

    test

--- ps1_test.ml
+++ ps1_test.ml
@@ -10,4 +10,6 @@ TEST_UNIT "is_mon_inc" =
 	assert_true (is_mon_inc [1;2;3])
 	assert_false (is_mon_inc [3;2;1])
 	assert_false (is_mon_inc [1;3;2])
-	assert_true (is_mon_inc [1;1;1])  *)
+	assert_true (is_mon_inc [1;1;1])  
+
+	new  *)
