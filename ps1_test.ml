b52df48bcabe69b47adcad25238fc5746b600f38
mfcripps <mfc84@cornell.edu>
2014-09-05 20:04:37 -0400

    hopefully this works

--- ps1_test.ml
+++ ps1_test.ml
@@ -2,7 +2,7 @@ open Assertions
 open Ps1
 
 TEST_UNIT "is_mon_inc" = 
-	assert_true  (is_mon_inc [])
+	assert_false  (is_mon_inc [])
 	(* assert_true (is_mon_inc [1])
 	assert_true (is_mon_inc [1;1])
 	assert_true (is_mon_inc [1;0])
@@ -12,4 +12,5 @@ TEST_UNIT "is_mon_inc" =
 	assert_false (is_mon_inc [1;3;2])
 	assert_true (is_mon_inc [1;1;1])  
 
-	new  *)
+HI ALEX*)
+
