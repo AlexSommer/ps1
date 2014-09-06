9e0e4b7ba95abffaedbe10cfa2bd5cccacb10644
AlexSommer <ajs455@cornell.edu>
2014-09-06 14:50:02 -0400

    finished rev_int with test cases

--- ps1.ml
+++ ps1.ml
@@ -1,3 +1,5 @@
+open String
+
 let rec is_mon_inc (list : int list) : bool = 
 	match list with 
 	| []-> true 
@@ -15,3 +17,16 @@ let rec is_unimodal (list : int list) : bool =
 	| [] -> true
 	| [x] -> true
 	| h::h2::t -> if h <= h2 then is_unimodal(h2::t) else is_mon_dec(h2::t)
+
+(* PLACE HOLDER FOR POWERSET FUNCTION*)
+
+
+let rec power (base : int) (raised : int) : int =
+	if raised <= 0 then 1 else base * (power base (raised-1))
+
+let rec rev_int (value : int) : int =
+	if value = 0 then 0 else 
+		let first = value mod 10 in
+		let len = length (string_of_int (abs (value))) in
+		first * (power 10 (len-1)) + (rev_int (value / 10))
+	
--- ps1_test.ml
+++ ps1_test.ml
@@ -14,6 +14,8 @@ TEST_UNIT "is_mon_inc_test8" = assert_false (is_mon_inc [1;3;2])
 TEST_UNIT "is_mon_inc_test9" = assert_true (is_mon_inc [1;1;1])
 TEST_UNIT "is_mon_inc_test10" = assert_false (is_mon_inc [1;2;9;0])
 
+(* ---------------------------------------------------- *)
+
 (* Tests for is_unimodal*)
 TEST_UNIT "is_unimodal_test1" = assert_true  (is_unimodal [])
 TEST_UNIT "is_unimodal_test2" = assert_true (is_unimodal [1])
@@ -31,6 +33,29 @@ TEST_UNIT "is_unimodal_test12" = assert_false (is_unimodal [1;2;1;2])
 TEST_UNIT "is_unimodal_test13" = assert_true (is_unimodal [0;1;0])
 TEST_UNIT "is_unimodal_test14" = assert_false (is_unimodal [1;2;3;3;2;1;2;2;-1])
 
+(* ---------------------------------------------------- *)
+
+(*Tests for rev_int*)
+TEST_UNIT "rev_int_test1" = assert_true ((rev_int 1234) = 4321)
+TEST_UNIT "rev_int_test2" = 
+	(* let () = print_int (rev_int (-1234)) in *)
+	assert_true ((rev_int (-1234)) = (-4321))
+TEST_UNIT "rev_int_test3" = assert_true ((rev_int 0) = 0)
+TEST_UNIT "rev_int_test5" = 
+	(* let () = print_int (rev_int (-10)) in *)
+	assert_true ((rev_int (-10)) = (-1))
+TEST_UNIT "rev_int_test6" = assert_true ((rev_int 11111) = 11111)
+TEST_UNIT "rev_int_test7" = assert_true ((rev_int 4) = 4)
+
+let () = print_int (rev_int 1073741823)
+
+(* 
+TEST_UNIT "rev_int_test8" = assert_true ((rev_int 1234) = 4321)
+TEST_UNIT "rev_int_test9" = assert_true ((rev_int 1234) = 4321)
+TEST_UNIT "rev_int_test10" = assert_true ((rev_int 1234) = 4321)
+TEST_UNIT "rev_int_test11" = assert_true ((rev_int 1234) = 4321)
+TEST_UNIT "rev_int_test12" = assert_true ((rev_int 1234) = 4321)
+ *)
 
 
 
