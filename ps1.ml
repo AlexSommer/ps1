1a2adac4786d8687188ceeebf84f0957ad2a6581
AlexSommer <ajs455@cornell.edu>
2014-09-06 02:32:54 -0400

    Why the hell am I doing this on a Friday night...?

--- ps1.ml
+++ ps1.ml
@@ -1,18 +1,17 @@
-let rec is_mon_inc (list:int list):bool = 
+let rec is_mon_inc (list : int list) : bool = 
 	match list with 
-	[]-> true 
+	| []-> true 
 	| [x] -> true 
 	| h::h2::t -> if h <= h2 then is_mon_inc(h2::t) else false
 
-<<<<<<< HEAD
-	(*hello
+let rec is_mon_dec (list : int list) : bool =
+	match list with
+	| [] -> true
+	| [x] -> true
+	| h::h2::t -> if h>=h2 then is_mon_dec(h2::t) else false
 
-
-
-
-
-
-	again*)
-=======
-	(*try # 2*)
->>>>>>> 5d41097cf86a18af182315a2d031f97e3141b2aa
+let rec is_unimodal (list : int list) : bool =
+	match list with
+	| [] -> true
+	| [x] -> true
+	| h::h2::t -> if h <= h2 then is_unimodal(h2::t) else is_mon_dec(h2::t)
--- ps1_test.ml
+++ ps1_test.ml
@@ -1,16 +1,38 @@
 open Assertions
 open Ps1
 
-TEST_UNIT "is_mon_inc" = 
-	assert_false  (is_mon_inc [])
-	(* assert_true (is_mon_inc [1])
-	assert_true (is_mon_inc [1;1])
-	assert_true (is_mon_inc [1;0])
-	assert_false (is_mon_inc [0;1])
-	assert_true (is_mon_inc [1;2;3])
-	assert_false (is_mon_inc [3;2;1])
-	assert_false (is_mon_inc [1;3;2])
-	assert_true (is_mon_inc [1;1;1])  
+(* Tests for is_mon_inc *)
+TEST_UNIT "is_mon_inc_test1" = assert_true  (is_mon_inc [])
+(*are we sure these are true above and below?*)
+TEST_UNIT "is_mon_inc_test2" = assert_true (is_mon_inc [1])
+TEST_UNIT "is_mon_inc_test3" = assert_true (is_mon_inc [1;1])
+TEST_UNIT "is_mon_inc_test4" = assert_false (is_mon_inc [1;0])
+TEST_UNIT "is_mon_inc_test5" = assert_true (is_mon_inc [0;1])
+TEST_UNIT "is_mon_inc_test6" = assert_true (is_mon_inc [1;2;3])
+TEST_UNIT "is_mon_inc_test7" = assert_false (is_mon_inc [3;2;1])
+TEST_UNIT "is_mon_inc_test8" = assert_false (is_mon_inc [1;3;2])
+TEST_UNIT "is_mon_inc_test9" = assert_true (is_mon_inc [1;1;1])
+TEST_UNIT "is_mon_inc_test10" = assert_false (is_mon_inc [1;2;9;0])
 
-HI ALEX*)
+(* Tests for is_unimodal*)
+TEST_UNIT "is_unimodal_test1" = assert_true  (is_unimodal [])
+TEST_UNIT "is_unimodal_test2" = assert_true (is_unimodal [1])
+TEST_UNIT "is_unimodal_test3" = assert_true (is_unimodal [1;1])
+TEST_UNIT "is_unimodal_test4" = assert_true (is_unimodal [1;0])
+TEST_UNIT "is_unimodal_test5" = assert_true (is_unimodal [0;1])
+TEST_UNIT "is_unimodal_test6" = assert_true (is_unimodal [1;2;3])
+TEST_UNIT "is_unimodal_test7" = assert_true (is_unimodal [3;2;1])
+TEST_UNIT "is_unimodal_test8" = assert_true (is_unimodal [1;3;2])
+TEST_UNIT "is_unimodal_test9" = assert_true (is_unimodal [1;1;1])
+TEST_UNIT "is_unimodal_test10" = assert_true (is_unimodal [1;2;9;0])
+(* new tests *)
+TEST_UNIT "is_unimodal_test11" = assert_true  (is_unimodal [1;2;3;3;2;0])
+TEST_UNIT "is_unimodal_test12" = assert_false (is_unimodal [1;2;1;2])
+TEST_UNIT "is_unimodal_test13" = assert_true (is_unimodal [0;1;0])
+TEST_UNIT "is_unimodal_test14" = assert_false (is_unimodal [1;2;3;3;2;1;2;2])
 
+
+
+
+
+let () = Pa_ounit_lib.Runtime.summarize()
